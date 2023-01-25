eadd6f3026e18f20c3cbf4896fb1a5db781dbb37
sirazhudin <sirazhudin@mail.ru>
2023-01-25 08:12:05 -0500

    v1

---
+++ src/main/java/ru/skillfactorybot/tgbot_j11/SkillFactoryTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.skillfactorybot.tgbot_j11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillFactoryTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillFactoryTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillfactorybot/tgbot_j11/SkillFactoryTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillfactorybot.tgbot_j11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillFactoryTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
